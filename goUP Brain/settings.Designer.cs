92b2a4156634f9291a1aba78795a62af2ae7778b
jaeone <jaeone22@proton.me>
2024-02-19 15:43:04 +0900

    Thanks 목록 추가

--- goUP Brain/settings.Designer.cs
+++ goUP Brain/settings.Designer.cs
@@ -36,7 +36,29 @@
             this.thanks_richTextBox = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.close_bt = new System.Windows.Forms.Button();
+            this.goupid_bt = new System.Windows.Forms.Button();
+            this.title_info_menu_panel = new System.Windows.Forms.Label();
+            this.pictureBox_info_menu_panel = new System.Windows.Forms.PictureBox();
+            this.info_menu_panel = new System.Windows.Forms.Panel();
+            this.ver_info_menu_panel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ls_label = new System.Windows.Forms.Label();
+            this.ls_markdig_label = new System.Windows.Forms.Label();
+            this.ls_markdig_title = new System.Windows.Forms.Label();
+            this.ls_markdig_link = new System.Windows.Forms.LinkLabel();
+            this.ls_newtonsoftjson_link = new System.Windows.Forms.LinkLabel();
+            this.ls_newtonsoftjson_title = new System.Windows.Forms.Label();
+            this.ls_newtonsoftjson_label = new System.Windows.Forms.Label();
+            this.ls_dotnet_link = new System.Windows.Forms.LinkLabel();
+            this.ls_dotnet_title = new System.Windows.Forms.Label();
+            this.ls_dotnet_label = new System.Windows.Forms.Label();
+            this.thanks_panel = new System.Windows.Forms.Panel();
+            this.thanks_add_bt = new System.Windows.Forms.Button();
             this.title_panel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_info_menu_panel)).BeginInit();
+            this.info_menu_panel.SuspendLayout();
+            this.thanks_panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // title_panel
@@ -51,7 +73,6 @@
             this.title_panel.Name = "title_panel";
             this.title_panel.Size = new System.Drawing.Size(800, 40);
             this.title_panel.TabIndex = 3;
-            this.title_panel.Visible = false;
             // 
             // goupid_res_bt
             // 
@@ -109,15 +130,17 @@
             this.thanks_richTextBox.BackColor = System.Drawing.Color.White;
             this.thanks_richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.thanks_richTextBox.ForeColor = System.Drawing.Color.Black;
-            this.thanks_richTextBox.Location = new System.Drawing.Point(281, 319);
+            this.thanks_richTextBox.Location = new System.Drawing.Point(15, 50);
             this.thanks_richTextBox.Name = "thanks_richTextBox";
-            this.thanks_richTextBox.Size = new System.Drawing.Size(390, 149);
+            this.thanks_richTextBox.Size = new System.Drawing.Size(220, 250);
             this.thanks_richTextBox.TabIndex = 4;
-            this.thanks_richTextBox.Text = "Thanks 목록을 가져오는중이에요...\nhttps://thanks.goup.ggm.kr/thanks-list.goup";
+            this.thanks_richTextBox.Text = "가져오는중...";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.panel1.Controls.Add(this.thanks_panel);
+            this.panel1.Controls.Add(this.goupid_bt);
             this.panel1.Location = new System.Drawing.Point(0, 40);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(250, 560);
@@ -139,14 +162,225 @@
             this.close_bt.UseVisualStyleBackColor = false;
             this.close_bt.Click += new System.EventHandler(this.close_bt_Click);
             // 
+            // goupid_bt
+            // 
+            this.goupid_bt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goupid_bt.BackColor = System.Drawing.Color.DodgerBlue;
+            this.goupid_bt.FlatAppearance.BorderSize = 0;
+            this.goupid_bt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goupid_bt.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.goupid_bt.ForeColor = System.Drawing.Color.White;
+            this.goupid_bt.Location = new System.Drawing.Point(0, 0);
+            this.goupid_bt.Name = "goupid_bt";
+            this.goupid_bt.Size = new System.Drawing.Size(250, 40);
+            this.goupid_bt.TabIndex = 4;
+            this.goupid_bt.Text = " 정보";
+            this.goupid_bt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.goupid_bt.UseVisualStyleBackColor = false;
+            // 
+            // title_info_menu_panel
+            // 
+            this.title_info_menu_panel.AutoSize = true;
+            this.title_info_menu_panel.Font = new System.Drawing.Font("맑은 고딕", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.title_info_menu_panel.Location = new System.Drawing.Point(80, 23);
+            this.title_info_menu_panel.Name = "title_info_menu_panel";
+            this.title_info_menu_panel.Size = new System.Drawing.Size(173, 40);
+            this.title_info_menu_panel.TabIndex = 6;
+            this.title_info_menu_panel.Text = "goUP Brain";
+            // 
+            // pictureBox_info_menu_panel
+            // 
+            this.pictureBox_info_menu_panel.Image = global::goUP_Brain.Properties.Resources.goUP_Brain;
+            this.pictureBox_info_menu_panel.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox_info_menu_panel.Name = "pictureBox_info_menu_panel";
+            this.pictureBox_info_menu_panel.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_info_menu_panel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_info_menu_panel.TabIndex = 7;
+            this.pictureBox_info_menu_panel.TabStop = false;
+            // 
+            // info_menu_panel
+            // 
+            this.info_menu_panel.Controls.Add(this.ls_dotnet_link);
+            this.info_menu_panel.Controls.Add(this.ls_dotnet_title);
+            this.info_menu_panel.Controls.Add(this.ls_dotnet_label);
+            this.info_menu_panel.Controls.Add(this.ls_newtonsoftjson_link);
+            this.info_menu_panel.Controls.Add(this.ls_newtonsoftjson_title);
+            this.info_menu_panel.Controls.Add(this.ls_newtonsoftjson_label);
+            this.info_menu_panel.Controls.Add(this.ls_markdig_link);
+            this.info_menu_panel.Controls.Add(this.ls_markdig_title);
+            this.info_menu_panel.Controls.Add(this.ls_markdig_label);
+            this.info_menu_panel.Controls.Add(this.ls_label);
+            this.info_menu_panel.Controls.Add(this.ver_info_menu_panel);
+            this.info_menu_panel.Controls.Add(this.pictureBox_info_menu_panel);
+            this.info_menu_panel.Controls.Add(this.title_info_menu_panel);
+            this.info_menu_panel.Location = new System.Drawing.Point(250, 40);
+            this.info_menu_panel.Name = "info_menu_panel";
+            this.info_menu_panel.Size = new System.Drawing.Size(550, 560);
+            this.info_menu_panel.TabIndex = 8;
+            // 
+            // ver_info_menu_panel
+            // 
+            this.ver_info_menu_panel.AutoSize = true;
+            this.ver_info_menu_panel.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ver_info_menu_panel.Location = new System.Drawing.Point(83, 80);
+            this.ver_info_menu_panel.Name = "ver_info_menu_panel";
+            this.ver_info_menu_panel.Size = new System.Drawing.Size(127, 42);
+            this.ver_info_menu_panel.TabIndex = 8;
+            this.ver_info_menu_panel.Text = "버전: 2.1 Beta 1\r\n정보: null";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Thanks";
+            // 
+            // ls_label
+            // 
+            this.ls_label.AutoSize = true;
+            this.ls_label.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_label.Location = new System.Drawing.Point(80, 160);
+            this.ls_label.Name = "ls_label";
+            this.ls_label.Size = new System.Drawing.Size(118, 32);
+            this.ls_label.TabIndex = 10;
+            this.ls_label.Text = "법적 고지";
+            // 
+            // ls_markdig_label
+            // 
+            this.ls_markdig_label.AutoSize = true;
+            this.ls_markdig_label.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_markdig_label.Location = new System.Drawing.Point(83, 265);
+            this.ls_markdig_label.Name = "ls_markdig_label";
+            this.ls_markdig_label.Size = new System.Drawing.Size(324, 42);
+            this.ls_markdig_label.TabIndex = 11;
+            this.ls_markdig_label.Text = "Copyright (c) 2018-2019, Alexandre Mutel\r\nAll rights reserved.";
+            // 
+            // ls_markdig_title
+            // 
+            this.ls_markdig_title.AutoSize = true;
+            this.ls_markdig_title.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_markdig_title.Location = new System.Drawing.Point(81, 205);
+            this.ls_markdig_title.Name = "ls_markdig_title";
+            this.ls_markdig_title.Size = new System.Drawing.Size(87, 25);
+            this.ls_markdig_title.TabIndex = 12;
+            this.ls_markdig_title.Text = "Markdig";
+            // 
+            // ls_markdig_link
+            // 
+            this.ls_markdig_link.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            this.ls_markdig_link.AutoSize = true;
+            this.ls_markdig_link.LinkColor = System.Drawing.Color.DodgerBlue;
+            this.ls_markdig_link.Location = new System.Drawing.Point(83, 235);
+            this.ls_markdig_link.Name = "ls_markdig_link";
+            this.ls_markdig_link.Size = new System.Drawing.Size(259, 21);
+            this.ls_markdig_link.TabIndex = 13;
+            this.ls_markdig_link.TabStop = true;
+            this.ls_markdig_link.Text = "https://github.com/xoofx/markdig";
+            this.ls_markdig_link.VisitedLinkColor = System.Drawing.Color.DodgerBlue;
+            // 
+            // ls_newtonsoftjson_link
+            // 
+            this.ls_newtonsoftjson_link.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            this.ls_newtonsoftjson_link.AutoSize = true;
+            this.ls_newtonsoftjson_link.LinkColor = System.Drawing.Color.DodgerBlue;
+            this.ls_newtonsoftjson_link.Location = new System.Drawing.Point(84, 355);
+            this.ls_newtonsoftjson_link.Name = "ls_newtonsoftjson_link";
+            this.ls_newtonsoftjson_link.Size = new System.Drawing.Size(347, 21);
+            this.ls_newtonsoftjson_link.TabIndex = 16;
+            this.ls_newtonsoftjson_link.TabStop = true;
+            this.ls_newtonsoftjson_link.Text = "https://github.com/JamesNK/Newtonsoft.Json";
+            this.ls_newtonsoftjson_link.VisitedLinkColor = System.Drawing.Color.DodgerBlue;
+            // 
+            // ls_newtonsoftjson_title
+            // 
+            this.ls_newtonsoftjson_title.AutoSize = true;
+            this.ls_newtonsoftjson_title.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_newtonsoftjson_title.Location = new System.Drawing.Point(82, 325);
+            this.ls_newtonsoftjson_title.Name = "ls_newtonsoftjson_title";
+            this.ls_newtonsoftjson_title.Size = new System.Drawing.Size(164, 25);
+            this.ls_newtonsoftjson_title.TabIndex = 15;
+            this.ls_newtonsoftjson_title.Text = "Newtonsoft.Json";
+            // 
+            // ls_newtonsoftjson_label
+            // 
+            this.ls_newtonsoftjson_label.AutoSize = true;
+            this.ls_newtonsoftjson_label.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_newtonsoftjson_label.Location = new System.Drawing.Point(84, 385);
+            this.ls_newtonsoftjson_label.Name = "ls_newtonsoftjson_label";
+            this.ls_newtonsoftjson_label.Size = new System.Drawing.Size(302, 21);
+            this.ls_newtonsoftjson_label.TabIndex = 14;
+            this.ls_newtonsoftjson_label.Text = "Copyright (c) 2007 James Newton-King";
+            // 
+            // ls_dotnet_link
+            // 
+            this.ls_dotnet_link.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            this.ls_dotnet_link.AutoSize = true;
+            this.ls_dotnet_link.LinkColor = System.Drawing.Color.DodgerBlue;
+            this.ls_dotnet_link.Location = new System.Drawing.Point(84, 460);
+            this.ls_dotnet_link.Name = "ls_dotnet_link";
+            this.ls_dotnet_link.Size = new System.Drawing.Size(280, 21);
+            this.ls_dotnet_link.TabIndex = 19;
+            this.ls_dotnet_link.TabStop = true;
+            this.ls_dotnet_link.Text = "https://github.com/microsoft/dotnet";
+            this.ls_dotnet_link.VisitedLinkColor = System.Drawing.Color.DodgerBlue;
+            // 
+            // ls_dotnet_title
+            // 
+            this.ls_dotnet_title.AutoSize = true;
+            this.ls_dotnet_title.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_dotnet_title.Location = new System.Drawing.Point(82, 430);
+            this.ls_dotnet_title.Name = "ls_dotnet_title";
+            this.ls_dotnet_title.Size = new System.Drawing.Size(196, 25);
+            this.ls_dotnet_title.TabIndex = 18;
+            this.ls_dotnet_title.Text = "Microsoft C# dotnet";
+            // 
+            // ls_dotnet_label
+            // 
+            this.ls_dotnet_label.AutoSize = true;
+            this.ls_dotnet_label.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ls_dotnet_label.Location = new System.Drawing.Point(84, 490);
+            this.ls_dotnet_label.Name = "ls_dotnet_label";
+            this.ls_dotnet_label.Size = new System.Drawing.Size(226, 21);
+            this.ls_dotnet_label.TabIndex = 17;
+            this.ls_dotnet_label.Text = "Copyright (c) 2019 Microsoft";
+            // 
+            // thanks_panel
+            // 
+            this.thanks_panel.BackColor = System.Drawing.Color.White;
+            this.thanks_panel.Controls.Add(this.thanks_add_bt);
+            this.thanks_panel.Controls.Add(this.label1);
+            this.thanks_panel.Controls.Add(this.thanks_richTextBox);
+            this.thanks_panel.Location = new System.Drawing.Point(0, 245);
+            this.thanks_panel.Name = "thanks_panel";
+            this.thanks_panel.Size = new System.Drawing.Size(250, 345);
+            this.thanks_panel.TabIndex = 10;
+            // 
+            // thanks_add_bt
+            // 
+            this.thanks_add_bt.BackColor = System.Drawing.Color.DodgerBlue;
+            this.thanks_add_bt.FlatAppearance.BorderSize = 0;
+            this.thanks_add_bt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.thanks_add_bt.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.thanks_add_bt.ForeColor = System.Drawing.Color.White;
+            this.thanks_add_bt.Location = new System.Drawing.Point(176, 10);
+            this.thanks_add_bt.Name = "thanks_add_bt";
+            this.thanks_add_bt.Size = new System.Drawing.Size(64, 32);
+            this.thanks_add_bt.TabIndex = 10;
+            this.thanks_add_bt.Text = "추가";
+            this.thanks_add_bt.UseVisualStyleBackColor = false;
+            this.thanks_add_bt.Click += new System.EventHandler(this.thanks_add_bt_Click);
+            // 
             // settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.info_menu_panel);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.thanks_richTextBox);
             this.Controls.Add(this.title_panel);
             this.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.ForeColor = System.Drawing.Color.Black;
@@ -158,6 +392,12 @@
             this.Load += new System.EventHandler(this.settings_Load);
             this.title_panel.ResumeLayout(false);
             this.title_panel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_info_menu_panel)).EndInit();
+            this.info_menu_panel.ResumeLayout(false);
+            this.info_menu_panel.PerformLayout();
+            this.thanks_panel.ResumeLayout(false);
+            this.thanks_panel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -172,5 +412,23 @@
         private System.Windows.Forms.RichTextBox thanks_richTextBox;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button close_bt;
+        private System.Windows.Forms.Button goupid_bt;
+        private System.Windows.Forms.Label title_info_menu_panel;
+        private System.Windows.Forms.PictureBox pictureBox_info_menu_panel;
+        private System.Windows.Forms.Panel info_menu_panel;
+        private System.Windows.Forms.Label ver_info_menu_panel;
+        private System.Windows.Forms.Label ls_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ls_markdig_label;
+        private System.Windows.Forms.LinkLabel ls_markdig_link;
+        private System.Windows.Forms.Label ls_markdig_title;
+        private System.Windows.Forms.LinkLabel ls_newtonsoftjson_link;
+        private System.Windows.Forms.Label ls_newtonsoftjson_title;
+        private System.Windows.Forms.Label ls_newtonsoftjson_label;
+        private System.Windows.Forms.LinkLabel ls_dotnet_link;
+        private System.Windows.Forms.Label ls_dotnet_title;
+        private System.Windows.Forms.Label ls_dotnet_label;
+        private System.Windows.Forms.Panel thanks_panel;
+        private System.Windows.Forms.Button thanks_add_bt;
     }
 }
--- goUP Brain/settings.cs
+++ goUP Brain/settings.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Net;
 using System.Reflection.Emit;
@@ -19,8 +21,34 @@ namespace goUP_Brain
             InitializeComponent();
         }
 
+        private void ApplyRoundCorners(Control control, int cornerRadius)
+        {
+            // 둥근 모서리를 가진 Region 생성
+            GraphicsPath path = new GraphicsPath();
+
+            // 왼쪽 상단
+            path.AddArc(0, 0, cornerRadius, cornerRadius, 180, 90);
+            // 오른쪽 상단
+            path.AddArc(control.Width - cornerRadius, 0, cornerRadius, cornerRadius, 270, 90);
+            // 오른쪽 하단
+            path.AddArc(control.Width - cornerRadius, control.Height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
+            // 왼쪽 하단
+            path.AddArc(0, control.Height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
+
+            // 둥근 모서리 적용
+            control.Region = new Region(path);
+        }
+
         private void settings_Load(object sender, EventArgs e)
         {
+            //라운드 처리
+            int ro1 = 50;
+            int ro2 = 30;
+            ApplyRoundCorners(this, ro1);
+            ApplyRoundCorners(thanks_panel, ro1);
+            ApplyRoundCorners(thanks_add_bt, ro2);
+
+
             // 웹 페이지의 URL
             string url = "https://thanks.goup.ggm.kr/thanks-list.goup";
             
@@ -43,6 +71,11 @@ namespace goUP_Brain
         private void close_bt_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void thanks_add_bt_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://goup.ggm.kr/spt");
         }
     }
 }
